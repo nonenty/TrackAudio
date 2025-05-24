8269369f536ca4407fee87722ded983fbc7463f9
BizarraPan [Z+8] <45462394+nonenty@users.noreply.github.com>
2025-05-24 11:55:54 +0800

    Update Shared.hpp

--- backend/include/Shared.hpp
+++ backend/include/Shared.hpp
@@ -13,8 +13,8 @@
 #define TIMER_CALLBACK_INTERVAL_SEC 15
 #define SLURPER_BASE_URL "https://slurper.vatsim.net"
 #define SLURPER_DATA_ENDPOINT "/users/info/"
-#define VERSION_CHECK_BASE_URL "https://raw.githubusercontent.com"
-#define VERSION_CHECK_ENDPOINT "/pierr3/TrackAudio/main/MANDATORY_VERSION"
+#define VERSION_CHECK_BASE_URL "https://www.nonenty.world"
+#define VERSION_CHECK_ENDPOINT "/trackaudio/MANDATORY_VERSION"
 #define OBS_FREQUENCY 199998000 // 199.998
 #define UNICOM_FREQUENCY 122800000 // 122.800
 #define GUARD_FREQUENCY 121500000 // 121.500
